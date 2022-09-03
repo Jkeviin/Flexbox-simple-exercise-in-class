544dc0975e2b792cc9d98a16eb77f06a23b7064f
Kevin <slash2130kevin@gmail.com>
2022-09-03 00:20:35 -0500

    ejercicio muy basico terminado

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,35 @@
 
 
 window.addEventListener("DOMContentLoaded", ()=>{
-    let change_color = document.getElementById("change-color");
+    let change_color = document.getElementsByClassName("color-change");
+    let btnChange = document.getElementById("change-color");
     let input_color = document.getElementById("input-color");
-    let card = document.querySelector(".card");
 
-    change_color.addEventListener("click", ()=>{
-        color.style.backgroundColor = input_color.value;
+
+    let border = 10;
+
+
+    btnChange.addEventListener("click", ()=>{
+        for(let i = 0; i < change_color.length; i++){
+            change_color[i].style.backgroundColor = input_color.value;
+        }
     });
+
+    let change_radius_max = document.getElementById("max");
+    let change_radius_min = document.getElementById("min");
+
+    change_radius_max.addEventListener("click", ()=>{
+        border = border + 1;
+        document.getElementById("card").style.borderRadius = border + "px";
+        document.getElementById("header").style.borderRadius = `${border}px ${border}px 0 0`;
+    });
+
+    change_radius_min.addEventListener("click", ()=>{
+        border = border - 1;
+        document.getElementById("card").style.borderRadius = border + "px";
+        document.getElementById("header").style.borderRadius = `${border}px ${border}px 0 0`;
+    });
+
 
 })
 
